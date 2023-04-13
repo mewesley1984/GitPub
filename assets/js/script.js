2d5ea45776e3b5d46cac6430e7da7d342a87a635
Matt  Wesley <mattwesley@Matts-MacBook-Air.local>
2023-04-12 21:23:37 -0400

    added JS for search form, added selector for search form, created funtion for keydwon event for search form

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var searchEl=document.getElementById("search")
+
+function clickPress(event) {
+    if (event.key === "Enter") {
+        console.log("do something")
+    }
+}
+
+function musicSearch() {
+
+
+}
