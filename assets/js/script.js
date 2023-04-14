d385b518c9c942d172b6637dd4bab73a7cdbf112
Chris Messa <blindsun95@gmail.com>
2023-04-13 22:15:01 -0500

    selected api data points

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var ticketMasterAPIKey = '9daAJhjhZVxP9AAiMXhhIxjkZhBwKooJ';
 function clickPress(event) {
     if (event.key === "Enter") {
         city = searchEl.value;
-        var ticketmasterQuery = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=${city}&apikey=${ticketMasterAPIKey}`;
+        var ticketmasterQuery = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&city=${city}&apikey=${ticketMasterAPIKey}`;
         function testApi() {
             fetch(ticketmasterQuery, {
                 mode: 'cors', 
@@ -15,7 +15,13 @@ function clickPress(event) {
                 return response.json();
             })
             .then((data) => {
-                console.log(data);
+                for (var i = 0; i < data._embedded.events.length; i++) {
+                    console.log(data._embedded.events[i]);
+                    var eventDiv = document.createElement("div")
+                    eventDiv.id = i;
+                    document.body.appendChild(eventDiv);
+                    eventDiv.textContent = `${data._embedded.events[i].name} - ${data._embedded.events[i]._embedded.venues[0].name} Date: ${data._embedded.events[i].dates.start.localDate}`;
+                }
             })}
         
             testApi();
