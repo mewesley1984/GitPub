63d5368458a83dc2a4e125a8910d8012bf435468
Chris Messa <blindsun95@gmail.com>
2023-04-15 14:13:45 -0500

    fetched breweries from venue location

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ function clickPress(event) {
     if (event.key === "Enter") {
         city = searchEl.value;
         var ticketmasterQuery = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&city=${city}&apikey=${ticketMasterAPIKey}`;
+
+
         function eventsQuery() {
             fetch(ticketmasterQuery, {
                 mode: 'cors', 
@@ -29,9 +31,6 @@ function musicSearch() {
 }
 
 function createEventList(searchData) {
-    var eventListEl = document.createElement("ol");
-    document.body.appendChild(eventListEl);
-    var currentEvent = 0;
     for (var i = 0; i < eventMainEL.length; i++) {
         // var eventLi = document.createElement("li")
         
@@ -45,8 +44,34 @@ function createEventList(searchData) {
         var eventVenue = event._embedded.venues[0]
         var eventName = event.name
         var venueName = eventVenue.name
-
+        var venueLat = eventVenue.location.latitude;
+        var venueLon = eventVenue.location.longitude;
+        function getBreweries() {
+            fetch(`https://api.openbrewerydb.org/v1/breweries?by_dist=${venueLat},${venueLon}&per_page=3`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+        }
+        getBreweries();
         eventMainEL[i].textContent = `${eventName} - ${venueName} Date: ${date}`
     }
 }
 
+// function getBreweries(venueData) {
+//     var geocodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=b4b306d022e640a29cb62888f869d314`
+//     geoCode();
+//     function geoCode()  {
+//         fetch(geocodeAPI, {
+//             mode: 'cors',
+//         })
+//         .then ((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             venueData
+//         })
+//     }
+// }
